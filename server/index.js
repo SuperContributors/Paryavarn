80ccbd27dc720b20e5018dabe93e6ece01b49897
Bhavya <bhavya.sachdeva.58@gmail.com>
2022-02-04 00:09:12 +0530

    Server setup done

--- server/index.js
+++ server/index.js
@@ -3,6 +3,11 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 const PORT = process.env.PORT || 3233;
 app.get("/", (req, res) => {
   res.send("Api is running.");
