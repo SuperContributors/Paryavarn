6b0f39d9adf020f6d6ed5dded82ffcf5ce83e036
Bhavya <bhavya.sachdeva.58@gmail.com>
2022-02-04 20:30:16 +0530

    Api-setup-done

---
+++ server/country.js
@@ -0,0 +1,33 @@
+
+module.exports = [
+  
+  {
+       "state_uts": "Andhra Pradesh", 
+       "geographical_area": 162968, 
+       "forest_cover": 28147, 
+       "tree_cover": 3753, 
+       "total_forest_and_tree_cover": 31900,
+        "percentage_of_geographical_area": 19.57 
+}, 
+{        "state_uts": "Arunachal Pradesh",
+         "geographical_area": 83743, 
+         "forest_cover": 66964, 
+         "tree_cover": 807, 
+         "total_forest_and_tree_cover": 67771,
+        "percentage_of_geographical_area": 80.93
+},
+{          "state_uts": "Assam", 
+            "geographical_area": 78438, 
+            "forest_cover": 28105, 
+            "tree_cover": 1496, 
+            "total_forest_and_tree_cover": 29601, 
+            "percentage_of_geographical_area": 37.74 
+}, 
+{           "state_uts": "Bihar", 
+            "geographical_area": 94163,
+            "forest_cover": 7299, 
+            "tree_cover": 2263,
+            "total_forest_and_tree_cover": 9562, 
+            "percentage_of_geographical_area": 10.15 
+},
+];
