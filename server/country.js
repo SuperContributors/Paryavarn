b9a6a51e4456f151b6645fc868c415b6fe6b15f4
Aayush Sharma <aayusharma1401@gmail.com>
2022-02-05 00:18:29 +0530

    US States in progress

--- server/country.js
+++ server/country.js
@@ -3,18 +3,188 @@ const country = [
     id: "US",
     states: [
       {
-        state: "Andhra Pradesh",
-        totalArea: 162968,
-        landCover: 131068,
-        forestCover: 31900,
-        percentageOfForestCover: 19.57,
+        state: "Maine",
+        totalArea: 91646,
+        landCover: 9990,
+        forestCover: 81656,
+        percentageOfForestCover: 89.10,
+        "check":1
       },
       {
-        state: "Andhra Pradesh",
-        totalArea: 162968,
-        landCover: 131068,
-        forestCover: 31900,
-        percentageOfForestCover: 19.57,
+        state: "New Hampshire",
+        totalArea: 24214,
+        landCover: 3797,
+        forestCover: 20417,
+        percentageOfForestCover: 84.32,
+        "check":2
+      },
+      {
+        state: "American Samoa",
+        totalArea: 200,
+        landCover: 38.32,
+        forestCover: 161.68,
+        percentageOfForestCover: 80.84,
+        "check":3
+      },
+      {
+        state: "Northern Mariana Islands",
+        totalArea: 464,
+        landCover: 91.09,
+        forestCover: 372.91,
+        percentageOfForestCover: 80.37,
+        "check":4
+      },
+      {
+        state: "West Virginia",
+        totalArea: 62259,
+        landCover: 13069,
+        forestCover: 49190,
+        percentageOfForestCover: 79.01,
+        "check":5
+      },
+      {
+        state: "Vermont",
+        totalArea: 24905,
+        landCover: 5526.42,
+        forestCover: 19378.58,
+        percentageOfForestCover: 77.81,
+        "check":6
+      },
+      {
+        state: "Alabama",
+        totalArea: 135765,
+        landCover: 39955.63,
+        forestCover: 95809.36,
+        percentageOfForestCover: 70.57,
+        "check":7
+      },
+      {
+        state: "South Carolina",
+        totalArea: 82931,
+        landCover: 26380,
+        forestCover: 56550.64,
+        percentageOfForestCover: 68.19,
+        "check":8
+      },
+      {
+        state: "Georgia",
+        totalArea: 69700,
+        landCover: 22806,
+        forestCover: 46894,
+        percentageOfForestCover: 67.28,
+        "check":9
+      },
+      {
+        state: "Mississippi",
+        totalArea: 125433,
+        landCover: 43813.74,
+        forestCover: 81619.25,
+        percentageOfForestCover: 65.07,
+        "check":10
+      },
+      {
+        state: "Virginia",
+        totalArea: 110786,
+        landCover: 41068.37,
+        forestCover: 69717.62,
+        percentageOfForestCover: 62.93,
+        "check":11
+      },
+      {
+        state: "New York",
+        totalArea: 141,300,
+        landCover: 52450,
+        forestCover: 88849,
+        percentageOfForestCover: 62.88,
+        "check":12
+      },
+      {
+        state: "Massachusetts",
+        totalArea: 27363,
+        landCover: 10789,
+        forestCover: 16573,
+        percentageOfForestCover: 60.57,
+        "check":13
+      },
+      {
+        state: "North Carolina",
+        totalArea: 139390,
+        landCover: 56132,
+        forestCover: 83257,
+        percentageOfForestCover: 59.73,
+        "check":14
+      },
+      {
+        state: "Pennsylvania",
+        totalArea: 119282,
+        landCover: 49382,
+        forestCover: 69899,
+        percentageOfForestCover: 58.60,
+        "check":15
+      },
+      {
+        state: "Virgin Islands (U.S.)",
+        totalArea: 346.4,
+        landCover: 148,
+        forestCover: 198,
+        percentageOfForestCover: 57.16,
+        "check":16
+      },
+      {
+        state: "Arkansas",
+        totalArea: 137733,
+        landCover: 60175.5,
+        forestCover: 77557,
+        percentageOfForestCover: 56.31,
+        "check":17
+      },
+      {
+        state: "Puerto Rico",
+        totalArea: 9104,
+        landCover: 4040,
+        forestCover: 5064,
+        percentageOfForestCover: 55.62,
+        "check":18
+      },
+      {
+        state: "Michigan",
+        totalArea: 250493,
+        landCover: 111168,
+        forestCover: 139324,
+        percentageOfForestCover: 55.62,
+        "check":19
+      },
+      {
+        state: "Connecticut",
+        totalArea: 14357,
+        landCover: 6426,
+        forestCover: 7931,
+        percentageOfForestCover: 55.24,
+        "check":20
+      },
+      {
+        state: "Rhode Island",
+        totalArea: 3144,
+        landCover: 1434,
+        forestCover: 1710,
+        percentageOfForestCover: 54.38,
+        "check":21
+      },
+      {
+        state: "Louisiana",
+        totalArea: 135658,
+        landCover: 63487,
+        forestCover: 72170,
+        percentageOfForestCover: 53.20,
+        "check":22
+      },
+      {
+        state: "Tennessee",
+        totalArea: 109247,
+        landCover: 51532,
+        forestCover: 57715,
+        percentageOfForestCover: 52.83,
+        "check":23
       },
     ],
   },
