bb93072c116ca8749965866d3e05dac21334123b
Chandan Kumar Saha <chandankumarshah8@gmail.com>
2022-02-04 22:35:34 +0530

    params working

--- server/country.js
+++ server/country.js
@@ -1,42 +1,308 @@
-const country= [{
-	"countries": [{
-			"id": "1",
-			"states": [{
-					"state": "Andhra Pradesh",
-					"totalArea": 162968,
-					"landCover": 131068,
-					"forestCover": 31900,
-					"percentageOfForestCover": 19.57
-				},
-				{
-					"state": "Andhra Pradesh",
-					"totalArea": 162968,
-					"landCover": 131068,
-					"forestCover": 31900,
-					"percentageOfForestCover": 19.57
-				}
-			]
-		},
-		{
-			"id": "2",
-			"states": [{
-					"state": "Andhra Pradesh",
-					"totalArea": 162968,
-					"landCover": 131068,
-					"forestCover": 31900,
-					"percentageOfForestCover": 19.57
-				},
-				{
-					"state": "Andhra Pradesh",
-					"totalArea": 162968,
-					"landCover": 131068,
-					"forestCover": 31900,
-					"percentageOfForestCover": 19.57
-				}
-			]
-		}
-	]
-} 
-]
+const country = [
+  {
+    id: "US",
+    states: [
+      {
+        state: "Andhra Pradesh",
+        totalArea: 162968,
+        landCover: 131068,
+        forestCover: 31900,
+        percentageOfForestCover: 19.57,
+      },
+      {
+        state: "Andhra Pradesh",
+        totalArea: 162968,
+        landCover: 131068,
+        forestCover: 31900,
+        percentageOfForestCover: 19.57,
+      },
+    ],
+  },
+  {
+    id: "UK",
+    states: [
+      {
+        state: "Andhra Pradesh",
+        totalArea: 162968,
+        landCover: 131068,
+        forestCover: 31900,
+        percentageOfForestCover: 19.57,
+      },
+      {
+        state: "Andhra Pradesh",
+        totalArea: 162968,
+        landCover: 131068,
+        forestCover: 31900,
+        percentageOfForestCover: 19.57,
+      },
+    ],
+  },
+  {
+    id: "Canada",
+    states: [
+      {
+        state: "Andhra Pradesh",
+        totalArea: 162968,
+        landCover: 131068,
+        forestCover: 31900,
+        percentageOfForestCover: 19.57,
+      },
+      {
+        state: "Andhra Pradesh",
+        totalArea: 162968,
+        landCover: 131068,
+        forestCover: 31900,
+        percentageOfForestCover: 19.57,
+      },
+    ],
+  },
+  {
+    id: "Australia",
+    states: [
+      {
+        state: "Andhra Pradesh",
+        totalArea: 162968,
+        landCover: 131068,
+        forestCover: 31900,
+        percentageOfForestCover: 19.57,
+      },
+      {
+        state: "Andhra Pradesh",
+        totalArea: 162968,
+        landCover: 131068,
+        forestCover: 31900,
+        percentageOfForestCover: 19.57,
+      },
+    ],
+  },
+  {
+    id: "Germany",
+    states: [
+      {
+        state: "Andhra Pradesh",
+        totalArea: 162968,
+        landCover: 131068,
+        forestCover: 31900,
+        percentageOfForestCover: 19.57,
+      },
+      {
+        state: "Andhra Pradesh",
+        totalArea: 162968,
+        landCover: 131068,
+        forestCover: 31900,
+        percentageOfForestCover: 19.57,
+      },
+    ],
+  },
+  {
+    id: "Italy",
+    states: [
+      {
+        state: "Andhra Pradesh",
+        totalArea: 162968,
+        landCover: 131068,
+        forestCover: 31900,
+        percentageOfForestCover: 19.57,
+      },
+      {
+        state: "Andhra Pradesh",
+        totalArea: 162968,
+        landCover: 131068,
+        forestCover: 31900,
+        percentageOfForestCover: 19.57,
+      },
+    ],
+  },
+  {
+    id: "India",
+    states: [
+      {
+        state: "Andhra Pradesh",
+        totalArea: 162968,
+        landCover: 131068,
+        forestCover: 31900,
+        percentageOfForestCover: 19.57,
+      },
+      {
+        state: "Andhra Pradesh",
+        totalArea: 162968,
+        landCover: 131068,
+        forestCover: 31900,
+        percentageOfForestCover: 19.57,
+      },
+    ],
+  },
+  {
+    id: "Switzerland",
+    states: [
+      {
+        state: "Andhra Pradesh",
+        totalArea: 162968,
+        landCover: 131068,
+        forestCover: 31900,
+        percentageOfForestCover: 19.57,
+      },
+      {
+        state: "Andhra Pradesh",
+        totalArea: 162968,
+        landCover: 131068,
+        forestCover: 31900,
+        percentageOfForestCover: 19.57,
+      },
+    ],
+  },
+  {
+    id: "Netherlands",
+    states: [
+      {
+        state: "Andhra Pradesh",
+        totalArea: 162968,
+        landCover: 131068,
+        forestCover: 31900,
+        percentageOfForestCover: 19.57,
+      },
+      {
+        state: "Andhra Pradesh",
+        totalArea: 162968,
+        landCover: 131068,
+        forestCover: 31900,
+        percentageOfForestCover: 19.57,
+      },
+    ],
+  },
+  {
+    id: "Sweden",
+    states: [
+      {
+        state: "Andhra Pradesh",
+        totalArea: 162968,
+        landCover: 131068,
+        forestCover: 31900,
+        percentageOfForestCover: 19.57,
+      },
+      {
+        state: "Andhra Pradesh",
+        totalArea: 162968,
+        landCover: 131068,
+        forestCover: 31900,
+        percentageOfForestCover: 19.57,
+      },
+    ],
+  },
+  {
+    id: "Japan",
+    states: [
+      {
+        state: "Andhra Pradesh",
+        totalArea: 162968,
+        landCover: 131068,
+        forestCover: 31900,
+        percentageOfForestCover: 19.57,
+      },
+      {
+        state: "Andhra Pradesh",
+        totalArea: 162968,
+        landCover: 131068,
+        forestCover: 31900,
+        percentageOfForestCover: 19.57,
+      },
+    ],
+  },
+  {
+    id: "Spain",
+    states: [
+      {
+        state: "Andhra Pradesh",
+        totalArea: 162968,
+        landCover: 131068,
+        forestCover: 31900,
+        percentageOfForestCover: 19.57,
+      },
+      {
+        state: "Andhra Pradesh",
+        totalArea: 162968,
+        landCover: 131068,
+        forestCover: 31900,
+        percentageOfForestCover: 19.57,
+      },
+    ],
+  },
+  {
+    id: "HongKong",
+    states: [
+      {
+        state: "Andhra Pradesh",
+        totalArea: 162968,
+        landCover: 131068,
+        forestCover: 31900,
+        percentageOfForestCover: 19.57,
+      },
+      {
+        state: "Andhra Pradesh",
+        totalArea: 162968,
+        landCover: 131068,
+        forestCover: 31900,
+        percentageOfForestCover: 19.57,
+      },
+    ],
+  },
+  {
+    id: "Belgium",
+    states: [
+      {
+        state: "Andhra Pradesh",
+        totalArea: 162968,
+        landCover: 131068,
+        forestCover: 31900,
+        percentageOfForestCover: 19.57,
+      },
+      {
+        state: "Andhra Pradesh",
+        totalArea: 162968,
+        landCover: 131068,
+        forestCover: 31900,
+        percentageOfForestCover: 19.57,
+      },
+    ],
+  },
+  {
+    id: "SouthKorea",
+    states: [
+      {
+        state: "Andhra Pradesh",
+        totalArea: 162968,
+        landCover: 131068,
+        forestCover: 31900,
+        percentageOfForestCover: 19.57,
+      },
+      {
+        state: "Andhra Pradesh",
+        totalArea: 162968,
+        landCover: 131068,
+        forestCover: 31900,
+        percentageOfForestCover: 19.57,
+      },
+    ],
+  },
+  {
+    id: "Denmark",
+    states: [
+      {
+        state: "Andhra Pradesh",
+        totalArea: 162968,
+        landCover: 131068,
+        forestCover: 31900,
+        percentageOfForestCover: 19.57,
+      },
+      {
+        state: "Andhra Pradesh",
+        totalArea: 162968,
+        landCover: 131068,
+        forestCover: 31900,
+        percentageOfForestCover: 19.57,
+      },
+    ],
+  },
+];
 
-module.exports = country
+module.exports = country;
