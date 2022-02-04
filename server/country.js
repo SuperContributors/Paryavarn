190a41e7175c37b5ee347fbc3208455dc4e45d7a
Aayush Sharma <aayusharma1401@gmail.com>
2022-02-04 22:11:27 +0530

    Json Format Created

--- server/country.js
+++ server/country.js
@@ -1,33 +1,42 @@
+const country= [{
+	"countries": [{
+			"id": "1",
+			"states": [{
+					"state": "Andhra Pradesh",
+					"totalArea": 162968,
+					"landCover": 131068,
+					"forestCover": 31900,
+					"percentageOfForestCover": 19.57
+				},
+				{
+					"state": "Andhra Pradesh",
+					"totalArea": 162968,
+					"landCover": 131068,
+					"forestCover": 31900,
+					"percentageOfForestCover": 19.57
+				}
+			]
+		},
+		{
+			"id": "2",
+			"states": [{
+					"state": "Andhra Pradesh",
+					"totalArea": 162968,
+					"landCover": 131068,
+					"forestCover": 31900,
+					"percentageOfForestCover": 19.57
+				},
+				{
+					"state": "Andhra Pradesh",
+					"totalArea": 162968,
+					"landCover": 131068,
+					"forestCover": 31900,
+					"percentageOfForestCover": 19.57
+				}
+			]
+		}
+	]
+} 
+]
 
-module.exports = [
-  
-  {
-       "state_uts": "Andhra Pradesh", 
-       "geographical_area": 162968, 
-       "forest_cover": 28147, 
-       "tree_cover": 3753, 
-       "total_forest_and_tree_cover": 31900,
-        "percentage_of_geographical_area": 19.57 
-}, 
-{        "state_uts": "Arunachal Pradesh",
-         "geographical_area": 83743, 
-         "forest_cover": 66964, 
-         "tree_cover": 807, 
-         "total_forest_and_tree_cover": 67771,
-        "percentage_of_geographical_area": 80.93
-},
-{          "state_uts": "Assam", 
-            "geographical_area": 78438, 
-            "forest_cover": 28105, 
-            "tree_cover": 1496, 
-            "total_forest_and_tree_cover": 29601, 
-            "percentage_of_geographical_area": 37.74 
-}, 
-{           "state_uts": "Bihar", 
-            "geographical_area": 94163,
-            "forest_cover": 7299, 
-            "tree_cover": 2263,
-            "total_forest_and_tree_cover": 9562, 
-            "percentage_of_geographical_area": 10.15 
-},
-];
+module.exports = country
